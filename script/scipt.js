48705117afc24305ec8d4e79eb3f53e8f34ca857
Nathan <chiracnathan59@gmail.com>
2024-10-01 20:00:58 +0200

    Commentaires JS + ajout bouton pause fonctionnel

--- script/scipt.js
+++ script/scipt.js
@@ -1,19 +1,27 @@
 let intervalle;
-let seconde;
+let seconde = 1500;
 let secondeDepart;
 let progressElement;
 let circleLength;
 let progress;
 let mins;
 let secs;
-let myBool = true;
+let phaseTravail = true;
 
 document.getElementById("play").addEventListener("click", function () {
     travailTimer();
+    if ( document.getElementById("play").textContent == "play_arrow" ){
+        document.getElementById("play").textContent = "pause";
+    } else if (document.getElementById("play").textContent == "pause") {
+        clearInterval(intervalle);
+        document.getElementById("play").textContent = "play_arrow";
+    }
 });
 
+/**
+ * Cette fonction lance le timer dpomodoro en declenchant un intervalle
+ */
 function travailTimer(){
-    seconde = 1200;
     secondeDepart = seconde;
     if (intervalle) {
         clearInterval(intervalle);
@@ -21,6 +29,9 @@ function travailTimer(){
     intervalle = setInterval(updateTimer, 1000);
 }
 
+/**
+ * Cette fonction modifie la valeur de la progress bar en fonction du temps de chaque phase (en seconde)
+ */
 function updateTimer() {
     seconde--;
     mins = Math.floor((seconde % 3600) / 60);
@@ -31,19 +42,20 @@ function updateTimer() {
         (secs < 10 ? "0" + secs : secs);
 
     progressElement = document.getElementById("progress-bar");
-    circleLength = progressElement.getTotalLength(); // 471 from the CSS stroke-dasharray
-    progress = (seconde / secondeDepart);  // Fraction of time remaining
-    progressElement.style.strokeDashoffset = circleLength * (1 - progress);  // Update offset
+    circleLength = progressElement.getTotalLength();
+    progress = (seconde / secondeDepart); // Permet d'avoir l'avancement de la progress bar
+    progressElement.style.strokeDashoffset = circleLength * (1 - progress); // On met a jour la progress bar
 
-    if (seconde == 0 && myBool == true) {
+
+    if (seconde == 0 && phaseTravail == true) { // On verifie que le timer est arrivé à terme et que l'on est dans la phase de travail alors on passe en phase de pause -> phaseTravail = false
         seconde = 300;
         secondeDepart = seconde;
         document.querySelector("html").style.backgroundColor = "green";
-        myBool = false;
-    } else if (seconde == 0 && myBool == false) {
-        seconde = 10;
+        phaseTravail = false;
+    } else if (seconde == 0 && phaseTravail == false) { // On verifie que le timer est arrivé à terme et que l'on est dans la phase de pause alors on passe en phase de travail -> phaseTravail = true
+        seconde = 1500;
         secondeDepart = seconde;
         document.querySelector("html").style.backgroundColor = "red";
-        myBool = true;
+        phaseTravail = true;
     }
 }
